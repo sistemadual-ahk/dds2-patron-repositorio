a20ed4ded521c70d4181d14dc7e3c32a17eeb86e
ezequieloscarescobar <ezequieloscarescobar2@gmail.com>
2020-06-18 13:54:00 -0300

    Update Repository

--- src/main/java/repositories/Repository.java
+++ src/main/java/repositories/Repository.java
@@ -1,15 +1,15 @@
 package repositories;
 
 import repositories.daos.IDAO;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Repository<T> {
     private IDAO<T> dao;
+    private Class<T> classToManage;
 
-    public Repository(IDAO<T> dao) {
+    public Repository(IDAO<T> dao, Class<T> classToManage) {
         this.dao = dao;
+        this.classToManage = classToManage;
     }
 
     public void setDao(IDAO<T> dao) {
@@ -20,12 +20,12 @@ public class Repository<T> {
         return this.dao.exist(id);
     }
 
-    public T find(int id, Class<T> clase){
-        return this.dao.find(id, clase);
+    public T find(int id){
+        return this.dao.find(id, this.classToManage);
     }
 
-    public List<T> findAll(Class<T> clase){
-        return this.dao.findAll(clase);
+    public List<T> findAll(){
+        return this.dao.findAll(this.classToManage);
     }
 
     public void delete(T object){
